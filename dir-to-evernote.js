bae7adeaf54a9d489a1a132d5b45458ce17c0c18
Reggie Zhang <reggie.zhang@sap.com>
2017-11-02 14:44:08 +0800

    commit by git-sync.sh

--- dir-to-evernote.js
+++ dir-to-evernote.js
@@ -114,9 +114,16 @@ function initSyncEntry(dirPath, filename, notebookName, rootDirName) {
   entry['notebook'] = notebookName;
   entry['attachments'] = [`${dirPath}/${filename}`];
   // entry['tags'] = [rootDirName, dirPath.split(path.sep).pop()];
-  let tagsArr = dirPath.split(path.sep);
-  tagsArr.unshift(rootDirName);
-  entry['tags'] = tagsArr;
+  entry['tags'] = [rootDirName];
+  let pathArr = dirPath.split(path.sep);
+  let tag = null;
+  while (tag = pathArr.pop()) {
+    if (tag === rootDirName) {
+      break;
+    } else {
+      entry['tags'].push(tag);
+    }
+  }
   return entry;
 }
 function completeSyncEntry(entry) {
